833d5c222887289da529d50c12c9648307ce5ecd
Haiman347 <51442807+Haiman347@users.noreply.github.com>
2019-06-30 22:32:56 +0700

    auto-installgit

---
+++ autogitclone.py
@@ -0,0 +1,71 @@
+import os, sys
+print ("autoinstallscrip")
+print ("")
+print ("Code writer:FEE")
+print ("") 
+print 
+print
+print ("Attack call&sms")
+print ("(1) call-sms")
+print ("(2) Spammer-grab ")
+print ("(3) Fakecall.")
+print ("")
+print
+print ("Attack ddos")
+print ("(4) hammer")
+print ("(5) Planetworkddos")
+print ("(6) ddoss")
+print ("")
+print ("View web data")
+print ("(7) sql ")
+print ("")
+print ("(0) exit ")
+print
+A = input ("install :")
+
+if A == "1" or A == "01":
+ print
+ os.system("git clone https://github.com/kongpun1/call-sms ")
+ print
+ print
+
+elif A == "2" or A == "02":
+ print
+ os.system("git clone  https://github.com/profionaldhim/Spammer-Grabe")
+ print
+ print
+
+elif A == "3" or A == "03":
+ print
+ os.system("git clone https://github.com/siputra12/fakecall ")
+ print
+ print
+
+elif A == "4" or A == "04":
+ print
+ os.system("git clone https://github.com/rk1342k/Hammer ")
+ print
+ print
+
+elif A == "5" or A == "05":
+ print
+ os.system("git clone https://github.com/Hydra7/Planetwork-DDOS ")
+ print
+ print
+
+elif A == "6" or A == "06":
+ print
+ os.system("git clone https://github.com/PraneethKarnena/DDoS-Scripts ")
+ print
+ print
+
+elif A == "7" or A == "07":
+ print
+ os.system("git clone https://github.com/sqlmapproject/sqlmap ")
+ print
+ print
+
+elif A == "0" or A == "00":
+ sys.exit()
+
+print ("Code writer: FEE")
